7169aa94c9c13b1bdaa1e09289cc2c954c6d8016
Sipturak <milos.maksimovic54@gmail.com>
2024-09-06 14:22:20 +0200

    bye handler function added

--- src/main/java/com/example/springaccountsdemo/router/RouterConfiguration.java
+++ src/main/java/com/example/springaccountsdemo/router/RouterConfiguration.java
@@ -18,6 +18,7 @@ public class RouterConfiguration {
         return RouterFunctions
                 .route()
                 .GET("/hello", handler::hello)
+                .GET("/bye", handler::bye)
                 .build();
     }
 
@@ -26,6 +27,10 @@ public class RouterConfiguration {
 
         public Mono<ServerResponse> hello(ServerRequest request) {
             return new HelloHandlerFunction().handle(request);
+        }
+
+        public Mono<ServerResponse> bye(ServerRequest request) {
+            return new ByeHandlerFunction().handle(request);
         }
 
     }
@@ -41,6 +46,18 @@ public class RouterConfiguration {
                     .ok()
                     .contentType(MediaType.APPLICATION_JSON)
                     .bodyValue("Hello from handler function");
+        }
+    }
+
+    @NonNullApi
+    public static class ByeHandlerFunction implements HandlerFunction<ServerResponse> {
+
+        @Override
+        public Mono<ServerResponse> handle(@NonNull ServerRequest request) {
+            return ServerResponse
+                    .ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue("Bye from handler function");
         }
     }
 
